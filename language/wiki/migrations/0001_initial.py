113b56f0d011c051a7d26856ac3ac4d81c0dd7fc
m516 <m516@m516>
2015-11-20 16:30:36 +0800

    CH 9

--- language/wiki/migrations/0001_initial.py
+++ language/wiki/migrations/0001_initial.py
@@ -13,16 +13,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('name', models.CharField(unique=True, max_length=128)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=128, unique=True)),
+                ('views', models.IntegerField(default=0)),
+                ('likes', models.IntegerField(default=0)),
             ],
         ),
         migrations.CreateModel(
             name='Page',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=128)),
                 ('url', models.URLField()),
+                ('views', models.IntegerField(default=0)),
                 ('category', models.ForeignKey(to='wiki.Category')),
             ],
         ),
--- language/wiki/urls.py
+++ language/wiki/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^$', views.wiki, name='wiki'),
     url(r'^category/(?P<categoryName>[\w\-]+)/$', views.category, name='category'),
+    url(r'^addCategory/$', views.addCategory, name='addCategory'),
+
 ]
